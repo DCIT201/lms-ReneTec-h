3c5647b7ddbc2d9153271f658f651ef4fd8c8e40
ReneTec-h <ireneadu272@gmail.com>
2024-12-18 20:30:41 +0000

    Initial Commit

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,14 +1,18 @@
 /*
  * This should be your main class where all your objects will be created
  */
+
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        // Creating a new book
+        Book book1 = new Book("How I Got Tired of The School", "Irene Adu", 2024);
+
+        book1.Properties();
+
     }
+
 }
